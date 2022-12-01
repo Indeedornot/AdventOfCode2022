62b780d2410fc65b85ab231d6cd552ed34be6190
indeedornot <gravitybartek@gmail.com>
2022-12-01 18:05:59 +0100

    Finished Day 1

---
+++ Day1/Solution.cs
@@ -0,0 +1,32 @@
+﻿//https: //adventofcode.com/2022/day/1
+
+namespace AdventOfCode.Day1; 
+
+public class Day1 {
+  public static void Main() {
+    string[] data = File.ReadAllLines(@"C:\Users\Indeed\Desktop\Stuff\Repositories\c#\AdventOfCode\Day1\Input.txt");
+
+    var caloriesAll = new List<int>();
+
+    int temp = 0;
+    for (int i = 0; i < data.Length; i++){
+      if (data[i] == " " || data[i] == ""){
+        caloriesAll.Add(temp);
+        temp = 0;
+        continue;
+      }
+
+      temp += int.Parse(data[i]);
+    }
+
+    caloriesAll.Sort((a, b) => b - a);
+
+    int sum = 0;
+    for (int i = 0; i < 3; i++){
+      sum += caloriesAll[i];
+      Console.WriteLine($"{i} {caloriesAll[i]}");
+    }
+
+    Console.WriteLine(sum);   
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Use command 'dotnet run PATH_TO_FILE' to run the program.");
